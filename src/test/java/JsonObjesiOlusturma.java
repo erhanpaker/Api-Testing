7ad61c40c2c109f04c604cf51b49d202bf6ce4a9
erhan <erhanpaker@gmail.com>
2023-03-29 15:22:08 +0300

    Api Testing

---
+++ src/test/java/JsonObjesiOlusturma.java
@@ -0,0 +1,20 @@
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class JsonObjesiOlusturma {
+
+    @Test
+
+    public void test01(){
+
+        JSONObject obj1 = new JSONObject();
+        obj1.put("title", "ahmet");
+        obj1.put("body","merhaba");
+        obj1.put("userId","1");
+
+        System.out.println(obj1);
+
+
+    }
+}
