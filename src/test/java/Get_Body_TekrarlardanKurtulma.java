578dad2ff1c8f11f0f74ab2f84a9ed761d4a133a
erhan <erhanpaker@gmail.com>
2023-04-03 16:37:45 +0300

    ApiTessting

---
+++ src/test/java/Get_Body_TekrarlardanKurtulma.java
@@ -0,0 +1,24 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Get_Body_TekrarlardanKurtulma {
+
+    @Test
+
+    public void test01(){
+
+        String url="https://restful-booker.herokuapp.com/booking/10";
+
+        Response response = given().when().get(url);
+
+         response.then().assertThat().statusCode(200).contentType(ContentType.JSON)
+                 .body("firstname", Matchers.equalTo("susan"))
+                 .body("lastname",Matchers.equalTo("Jakson"))
+                 .
+
+    }
+}
