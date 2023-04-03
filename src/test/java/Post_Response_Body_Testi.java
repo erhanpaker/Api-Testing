cf873afb879a206c9b9b86abd9c828a8c6656159
erhan <erhanpaker@gmail.com>
2023-04-03 15:02:04 +0300

    ApiTessting

--- src/test/java/Post_Response_Body_Testi.java
+++ src/test/java/Post_Response_Body_Testi.java
@@ -1,5 +1,10 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class Post_Response_Body_Testi {
 
@@ -15,6 +20,19 @@ public class Post_Response_Body_Testi {
         requestBody.put("title","API");
         requestBody.put("body","API ogrenmek ne güzel");
         requestBody.put("userId","10");
+
+        Response response = given().contentType(ContentType.JSON)
+                .when().body(requestBody.toString())
+                .post(url);
+
+
+         response.then().assertThat()
+                 .statusCode(201)
+                 .contentType(ContentType.JSON)
+                 .body("title", Matchers.equalTo("API"))
+                 .body("userId",Matchers.lessThan(100))
+                 .body("body",Matchers.containsString("API"));
+
 
 
 
