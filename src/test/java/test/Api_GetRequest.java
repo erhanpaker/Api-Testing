0f065388e428f10a9f53d79a707078a29a6deeaf
erhan <erhanpaker@gmail.com>
2023-05-12 15:02:13 +0300

    api test

---
+++ src/test/java/test/Api_GetRequest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class Api_GetRequest {
+
+    @Test
+
+    public void gey01(){
+
+        String url ="https://jsonplaceholder.typicode.com/posts/44";
+
+        JSONObject expBody = new JSONObject();
+
+    }
+}
